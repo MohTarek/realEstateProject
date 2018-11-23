89ea8042f88a7d576d4bd57d17b0576d18b6babe
MohTarek <hamodelbrolosy@gmail.com>
2018-11-23 17:19:34 +0200

    Create PostQueries.php

---
+++ queries/PostQueries.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of PostQueries
+ *
+ * @author Mohamed
+ */
+class PostQueries {
+    //put your code here
+}
